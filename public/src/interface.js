b4c1e1d7c030d0efabbd2a057dcb48a65ce6e710
Frank Mckenna <fmckenna56330@gmail.com>
2021-07-12 17:00:22 +0100

    adding new li elements to the DOM tree

--- public/src/interface.js
+++ public/src/interface.js
@@ -5,12 +5,27 @@ document.addEventListener("DOMContentLoaded", () => {
 
   document.querySelector("#create-note").addEventListener("click", function(e) {
     const noteText = document.getElementById('note-text').value;
-
     e.preventDefault();
     myStorage.setItem('noteText', noteText);
-    // myStorage.getItem('noteText');
-    // console.log(noteText);
-    // console.log(myStorage.getItem('noteText'));
+    let note = myStorage.getItem('noteText');
+    let title;
+    if (note.length < 20) {
+      title = myStorage.getItem('noteText');
+    } else {
+      console.log('hello');
+      title = myStorage.getItem('noteText').slice(0, 19);
+    }
+
+    let newEl = document.createElement('li');
+    let newText = document.createTextNode(`${title}`);
+    newEl.appendChild(newText);
+    let listPosition = document.getElementsByTagName('ul')[0];
+    listPosition.appendChild(newEl);
+  })
+
+  document.querySelector('#note-list').addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log('ewrwfef');
   })
 
 });
