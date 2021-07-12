b4175dea1e52ab1f2078fd28e21a8b1df441c8c6
Cynthia Fu <yinnycfu@gmail.com>
2021-07-12 16:09:54 +0100

    lstores note text in sessionStorage

--- public/src/interface.js
+++ public/src/interface.js
@@ -1,5 +1,16 @@
 'use strict';
 
 document.addEventListener("DOMContentLoaded", () => {
-  
+  const myStorage = window.sessionStorage;
+
+  document.querySelector("#create-note").addEventListener("click", function(e) {
+    const noteText = document.getElementById('note-text').value;
+
+    e.preventDefault();
+    myStorage.setItem('noteText', noteText);
+    // myStorage.getItem('noteText');
+    // console.log(noteText);
+    // console.log(myStorage.getItem('noteText'));
+  })
+
 });
