b92401d1dd3164fe04fd84f66b32272f306b9dc5
Cynthia Fu <yinnycfu@gmail.com>
2021-07-13 17:46:11 +0100

    mnotes persist in window session storage and are displayed

--- public/src/interface.js
+++ public/src/interface.js
@@ -3,6 +3,10 @@
 let myStorage = window.sessionStorage;
 let elementId = myStorage.length;
 
+document.addEventListener("DOMContentLoaded", () => {
+  displayLinksFromStorage();
+})
+
 const storingNote = (elementId, noteText) => {
   myStorage.setItem(noteText, elementId)
 }
@@ -10,7 +14,7 @@ const storingNote = (elementId, noteText) => {
 const getTitle = () => {
   let note = myStorage.getItem(elementId);
   return note.length < 20 ? note : note.slice(0, 19)
-  }
+}
 
 const makeClickSubmitCreateLink = () => {
   elementId += 1;
@@ -31,5 +35,22 @@ const addingNewElement = (newEl, newText) => {
   newEl.setAttribute('id', elementId)
   let listPosition = document.getElementsByTagName('ul')[0];
   listPosition.appendChild(newEl);
+}
+
+const displayLinksFromStorage = () => {
+  for (let i = 1; i < elementId; i++) {
+    // Get the note
+
+    // Create the title and ensure 20 chars.
+    let note = myStorage.getItem(i);
+    let title = note.length < 20 ? note : note.slice(0, 19);
+
+    let existingEl = document.createElement('li');
+    let existingText = document.createTextNode(title);
+    existingEl.appendChild(existingText);
+    existingEl.setAttribute('id', i)
+    let listPosition = document.getElementsByTagName('ul')[0];
+    listPosition.appendChild(existingEl);
+  }
 }
 
