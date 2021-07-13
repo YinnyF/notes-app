8813bb8da2b160b07ac667140a8764a736ac78bb
Frank Mckenna <fmckenna56330@gmail.com>
2021-07-13 17:23:12 +0100

    refactored

--- public/src/interface.js
+++ public/src/interface.js
@@ -1,30 +1,35 @@
 'use strict';
 
-  let myStorage = window.sessionStorage;
-  let elementId = myStorage.length;
+let myStorage = window.sessionStorage;
+let elementId = myStorage.length;
 
-  const storingNote = (elementId, noteText) => {
-    myStorage.setItem(noteText, elementId)
+const storingNote = (elementId, noteText) => {
+  myStorage.setItem(noteText, elementId)
+}
+
+const getTitle = () => {
+  let note = myStorage.getItem(elementId);
+  return note.length < 20 ? note : note.slice(0, 19)
   }
 
-  const getTitle = () => {
-    let note = myStorage.getItem(elementId);
-    return note.length < 20 ? note : note.slice(0, 19)
-    }
-
-  const makeClickSubmitCreateLink = () => {
-    elementId += 1;
-    let noteText = document.getElementById('note-text').value;
-    storingNote(noteText, elementId);
-    createLink();
+const makeClickSubmitCreateLink = () => {
+  elementId += 1;
+  let noteText = document.getElementById('note-text').value;
+  storingNote(noteText, elementId);
+  createLink();
 }
 
 const createLink = () => {
   let title = getTitle();
   let newEl = document.createElement('li');
   let newText = document.createTextNode(title);
+  addingNewElement(newEl, newText);
+}
+
+const addingNewElement = (newEl, newText) => {
   newEl.appendChild(newText);
   newEl.setAttribute('id', elementId)
   let listPosition = document.getElementsByTagName('ul')[0];
   listPosition.appendChild(newEl);
 }
+
