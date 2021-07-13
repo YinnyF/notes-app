f0da5aa6ee146d7596771d0cd4b255d6da774d27
Frank Mckenna <fmckenna56330@gmail.com>
2021-07-13 17:17:13 +0100

    refactored

--- public/src/interface.js
+++ public/src/interface.js
@@ -1,31 +1,30 @@
 'use strict';
 
-document.addEventListener("DOMContentLoaded", () => {
-  const myStorage = window.sessionStorage;
+  let myStorage = window.sessionStorage;
+  let elementId = myStorage.length;
 
-  document.querySelector("#create-note").addEventListener("click", function(e) {
-    const noteText = document.getElementById('note-text').value;
-    e.preventDefault();
-    myStorage.setItem('noteText', noteText);
-    let note = myStorage.getItem('noteText');
-    let title;
-    if (note.length < 20) {
-      title = myStorage.getItem('noteText');
-    } else {
-      console.log('hello');
-      title = myStorage.getItem('noteText').slice(0, 19);
+  const storingNote = (elementId, noteText) => {
+    myStorage.setItem(noteText, elementId)
+  }
+
+  const getTitle = () => {
+    let note = myStorage.getItem(elementId);
+    return note.length < 20 ? note : note.slice(0, 19)
     }
 
-    let newEl = document.createElement('li');
-    let newText = document.createTextNode(`${title}`);
-    newEl.appendChild(newText);
-    let listPosition = document.getElementsByTagName('ul')[0];
-    listPosition.appendChild(newEl);
-  })
+  const makeClickSubmitCreateLink = () => {
+    elementId += 1;
+    let noteText = document.getElementById('note-text').value;
+    storingNote(noteText, elementId);
+    createLink();
+}
 
-  document.querySelector('#note-list').addEventListener("click", (e) => {
-    e.preventDefault();
-    console.log('ewrwfef');
-  })
-
-});
+const createLink = () => {
+  let title = getTitle();
+  let newEl = document.createElement('li');
+  let newText = document.createTextNode(title);
+  newEl.appendChild(newText);
+  newEl.setAttribute('id', elementId)
+  let listPosition = document.getElementsByTagName('ul')[0];
+  listPosition.appendChild(newEl);
+}
