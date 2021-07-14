ae244173c04831829d5176d1fde41a589b06ae4e
Frank Mckenna <fmckenna56330@gmail.com>
2021-07-14 16:41:02 +0100

    user can add an id

--- spec/storageSpec.js
+++ spec/storageSpec.js
@@ -6,3 +6,8 @@ it("can store a note", () => {
   testStorage.store("Message 1");
   expect(testStorage.get("Message 1")).toEq("Message 1");
 });
+
+it("can assign an id number", () => {
+  testStorage.store('Message 2');
+  expect(testStorage.get(2)).toEq("Message 2");
+});
--- src/storage.js
+++ src/storage.js
@@ -3,10 +3,11 @@
 class Storage {
   constructor() {
     this.myStorage = window.sessionStorage
+    this.numberOfNotes = this.myStorage.length
   }
 
   store(content) {
-    this.myStorage.setItem(content, content);
+    this.myStorage.setItem(this.numberOfNotes +1, content);
   }
 
   get(content) {
