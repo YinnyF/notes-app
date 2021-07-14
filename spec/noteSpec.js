5d569b6944798032d2fc73d44858f3d20ab07b66
Cynthia Fu <yinnycfu@gmail.com>
2021-07-15 00:17:39 +0100

    style test runner

--- spec/noteSpec.js
+++ spec/noteSpec.js
@@ -1,18 +1,20 @@
 'use strict';
 
-it("Note - can store a note", () => {
-  let testNote = new Note();
-  let content = "Message 1"
-  testNote.addNote(content);
+describe('Note', () => {
+  it("can store a note", () => {
+    let testNote = new Note();
+    let content = "Message 1"
+    testNote.addNote(content);
 
-  expect(testNote.getNote(1)).toEq(content);
-});
+    expect(testNote.getNote(1)).toEq(content);
+  });
 
-it("Note - can retrieve notes", () => {
-  let testNote = new Note();
-  testNote.addNote("Message 1");
-  let content = "Message 2"
-  testNote.addNote(content);
+  it("can retrieve notes", () => {
+    let testNote = new Note();
+    testNote.addNote("Message 1");
+    let content = "Message 2"
+    testNote.addNote(content);
 
-  expect(testNote.getNote(2)).toEq(content);
+    expect(testNote.getNote(2)).toEq(content);
+  });
 });
