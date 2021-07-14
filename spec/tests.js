455a172d46af6703cd46f885daf5d6c690fe2b2a
Cynthia Fu <yinnycfu@gmail.com>
2021-07-14 12:42:03 +0100

    all test labels displayed on testrunner

--- spec/tests.js
+++ spec/tests.js
@@ -7,3 +7,7 @@ it('2 + 2 is equal to 4', () => {
 it('2 + 3 is equal to 4', () => {
   expect(2+3).toEq(4);
 });
+
+it('2 + 4 is equal to 4', () => {
+  expect(2+4).toEq(4);
+});
--- src/note.js
+++ src/note.js
@@ -1,5 +1,5 @@
 'use strict';
 
-class Thermostat {
+class Note {
 
 }
