b3af0a1756a833467890bcfa4fa2511cb1cfd2dc
Cynthia Fu <yinnycfu@gmail.com>
2021-07-15 23:06:13 +0100

    testing notes app

--- spec/noteSpec.js
+++ spec/noteSpec.js
@@ -9,12 +9,21 @@ describe('Note', () => {
     expect(testNote.getNote(1)).toEq(content);
   });
 
-  it("can retrieve notes", () => {
+  it("can retrieve a note", () => {
     let testNote = new Note();
     testNote.addNote("Message 1");
     let content = "Message 2"
     testNote.addNote(content);
 
     expect(testNote.getNote(2)).toEq(content);
+  });
+
+  it("can retreive all notes as an array", () => {
+    let testNote = new Note();
+    testNote.addNote("Message 1");
+    let content = "Message 2"
+    testNote.addNote(content);
+
+    expect(testNote.getNotes()).toBeAnArray();
   });
 });
